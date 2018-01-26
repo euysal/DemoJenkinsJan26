accd40b05dc9eb493c942f71f66392abb2153fef
euysal <uysaleda@windowslive.com>
2018-01-26 02:39:11 -0800

    xxx

--- src/main/java/TryMe/TryAgain/App.java
+++ src/main/java/TryMe/TryAgain/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!xxxxxx" );
+        System.out.println( "xxxxxxHello World!xxxxxx" );
     }
 }
