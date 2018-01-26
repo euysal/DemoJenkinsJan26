64fdea466ff214965479a2039c04f38528afcd05
euysal <uysaleda@windowslive.com>
2018-01-26 02:32:15 -0800

    try me

---
+++ src/main/java/TryMe/TryAgain/App.java
@@ -0,0 +1,13 @@
+package TryMe.TryAgain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
